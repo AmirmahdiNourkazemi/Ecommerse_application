4cca1f3fc17af696d2f8879ea18822d93dd2de1e
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2022-12-08 11:39:46 +0330

    product design added

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:mobile_shop/constanse/const.dart';
 import 'package:mobile_shop/widgets/slider.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -19,42 +19,182 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffE5E5E5),
-      body: Column(
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Container(
-                height: 56,
-                width: 56,
-                decoration: ShapeDecoration(
-                  shadows: [
-                    BoxShadow(
-                      color: Colors.red,
-                      blurRadius: 30,
-                      spreadRadius: -6,
-                      offset: Offset(0.0, 15),
-                    )
-                  ],
-                  color: Colors.red,
-                  shape: ContinuousRectangleBorder(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 163,
+              width: 160,
+              decoration: BoxDecoration(
+                color: ColorPicker.whiteColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
                 ),
               ),
-              Icon(
-                Icons.mouse,
-                color: Colors.white,
-                size: 29,
-              )
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Text('data')
-        ],
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Stack(
+                    children: [
+                      Center(
+                        child: Container(
+                          height: 140,
+                          width: 79,
+                          child: Image.asset(
+                            'assets/images/iphone-13-pro-max-silver-select 1.png',
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        top: 10,
+                        right: 10,
+                        child: Image.asset('assets/images/icon_like.png'),
+                      ),
+                      Positioned(
+                        left: 10,
+                        bottom: 10,
+                        child: Container(
+                          height: 15,
+                          width: 25,
+                          decoration: BoxDecoration(
+                            color: ColorPicker.red,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(7.5),
+                            ),
+                          ),
+                          child: Center(
+                              child: Text(
+                            '٪۳',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontFamily: 'SM',
+                              fontSize: 10,
+                            ),
+                          )),
+                        ),
+                      )
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 10, bottom: 3),
+                    child: Text(
+                      'آیفون ۱۳ پرومکس',
+                      style: TextStyle(fontSize: 14, fontFamily: 'SB'),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 53,
+              width: 160,
+              decoration: BoxDecoration(
+                color: ColorPicker.blue,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'تومان',
+                      style: TextStyle(
+                        fontFamily: 'SM',
+                        color: Colors.white,
+                      ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '۴۶٬۰۰۰٬۰۰۰',
+                          style: TextStyle(
+                            decoration: TextDecoration.lineThrough,
+                            color: Colors.white,
+                            fontFamily: 'SM',
+                          ),
+                        ),
+                        Text(
+                          '۴۵٬۳۵۰٬۰۰۰',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'SM',
+                          ),
+                        ),
+                      ],
+                    ),
+                    Image.asset('assets/images/arrow2.png')
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  Padding listCategoryBuilder() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 40),
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: categoryHorizontalListView(),
+          );
+        },
+      ),
+    );
+  }
+
+  Column categoryHorizontalListView() {
+    return Column(
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              height: 56,
+              width: 56,
+              decoration: ShapeDecoration(
+                shadows: [
+                  BoxShadow(
+                    color: Colors.red,
+                    blurRadius: 25,
+                    spreadRadius: -15,
+                    offset: Offset(0.0, 15),
+                  )
+                ],
+                color: Colors.red,
+                shape: ContinuousRectangleBorder(
+                  borderRadius: BorderRadius.circular(40),
+                ),
+              ),
+            ),
+            Icon(
+              Icons.mouse,
+              color: Colors.white,
+              size: 29,
+            )
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'همه',
+          style: TextStyle(fontFamily: 'SB', fontSize: 17),
+        )
+      ],
     );
   }
 
---
+++ lib/constanse/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorPicker {
+  static const whiteColor = Color(0xfffFFFFFF);
+  static const blue = Color(0xfff3B5EDF);
+  static const red = Color(0xfffD02026);
+}
