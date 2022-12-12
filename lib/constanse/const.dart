b307b8a6aa485ecaa361819d6d5c4c3eeab4b3e2
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2022-12-12 22:49:55 +0330

    complete basket screen

--- lib/constanse/const.dart
+++ lib/constanse/const.dart
@@ -4,4 +4,6 @@ class ColorPicker {
   static const whiteColor = Color(0xfffFFFFFF);
   static const blue = Color(0xfff3B5EDF);
   static const red = Color(0xfffD02026);
+  static const green = Color(0xff1DB68B);
+  static const redLight = Color(0xffD02026);
 }
--- lib/screens/buyList_screen.dart
+++ lib/screens/buyList_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:mobile_shop/constanse/const.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({Key? key}) : super(key: key);
@@ -256,47 +259,355 @@ class _CartScreenState extends State<CartScreen> {
                 ),
               ),
               SliverToBoxAdapter(
-                  child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 44, vertical: 20),
-                child: Container(
-                  width: 340,
-                  height: 46,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(15),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 44, vertical: 20),
+                  child: Container(
+                    width: 340,
+                    height: 46,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
                     ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset('assets/images/forward_icon.png'),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              'مشاهده',
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontFamily: 'SB',
-                                color: Color(0xff3B5EDF),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset('assets/images/forward_icon.png'),
+                              SizedBox(
+                                width: 10,
                               ),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          ':مشخصات فنی',
-                          style: TextStyle(fontSize: 12, fontFamily: 'SB'),
-                        ),
-                      ],
+                              Text(
+                                'مشاهده',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontFamily: 'SB',
+                                  color: Color(0xff3B5EDF),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            ':مشخصات فنی',
+                            style: TextStyle(fontSize: 12, fontFamily: 'SB'),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ))
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 44),
+                  child: Container(
+                    width: 340,
+                    height: 46,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset('assets/images/forward_icon.png'),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                'مشاهده',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontFamily: 'SB',
+                                  color: Color(0xff3B5EDF),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            ':توضیحات محصول',
+                            style: TextStyle(fontSize: 12, fontFamily: 'SB'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 44, vertical: 20),
+                  child: Container(
+                    width: 340,
+                    height: 46,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset('assets/images/forward_icon.png'),
+                              SizedBox(
+                                width: 10,
+                              ),
+                              Text(
+                                'مشاهده',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontFamily: 'SB',
+                                  color: Color(0xff3B5EDF),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Stack(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(left: 60),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Color(0xff858585),
+                                    width: 0.5,
+                                  ),
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
+                                ),
+                                width: 26,
+                                height: 26,
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 45),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: Color(0xff858585), width: 0.5),
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
+                                ),
+                                width: 26,
+                                height: 26,
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 30),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Color(0xff858585),
+                                    width: 0.5,
+                                  ),
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
+                                ),
+                                width: 26,
+                                height: 26,
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 15),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Color(0xff858585),
+                                    width: 0.5,
+                                  ),
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
+                                ),
+                                width: 26,
+                                height: 26,
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(left: 0),
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Color(0xff858585),
+                                    width: 0.5,
+                                  ),
+                                  color: Colors.grey,
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(8),
+                                  ),
+                                ),
+                                width: 26,
+                                height: 26,
+                                child: Center(
+                                    child: Text(
+                                  '+10',
+                                  style: TextStyle(fontFamily: 'SB'),
+                                )),
+                              )
+                            ],
+                          ),
+                          Text(
+                            ':نظرات کاربران',
+                            style: TextStyle(fontSize: 12, fontFamily: 'SB'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Stack(
+                      alignment: AlignmentDirectional.bottomCenter,
+                      clipBehavior: Clip.none,
+                      children: [
+                        Container(
+                          height: 74,
+                          width: 140,
+                          decoration: BoxDecoration(
+                            color: ColorPicker.green,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 10,
+                          bottom: 0,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              child: Container(
+                                height: 53,
+                                width: 160,
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 10),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'تومان',
+                                        style: TextStyle(
+                                          fontFamily: 'SM',
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            '۴۶٬۰۰۰٬۰۰۰',
+                                            style: TextStyle(
+                                              decoration:
+                                                  TextDecoration.lineThrough,
+                                              color: Colors.white,
+                                              fontFamily: 'SM',
+                                            ),
+                                          ),
+                                          Text(
+                                            '۴۵٬۳۵۰٬۰۰۰',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontFamily: 'SM',
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                      Container(
+                                        width: 25,
+                                        height: 15,
+                                        decoration: BoxDecoration(
+                                          color: ColorPicker.redLight,
+                                          borderRadius: BorderRadius.all(
+                                            Radius.circular(15),
+                                          ),
+                                        ),
+                                        child: Padding(
+                                            padding: EdgeInsets.only(
+                                              top: 2,
+                                              left: 5,
+                                            ),
+                                            child: Text(
+                                              '%۵',
+                                              style: TextStyle(
+                                                color: Colors.white,
+                                                fontFamily: 'SM',
+                                                fontSize: 10,
+                                              ),
+                                            )),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Stack(
+                      alignment: AlignmentDirectional.bottomCenter,
+                      clipBehavior: Clip.none,
+                      children: [
+                        Positioned(
+                          child: Container(
+                            height: 60,
+                            width: 140,
+                            decoration: BoxDecoration(
+                              color: ColorPicker.blue,
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(15),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: 10,
+                          bottom: 0,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              child: Container(
+                                height: 53,
+                                width: 160,
+                                child: Center(
+                                  child: Text(
+                                    'افزودن به سبد خرید',
+                                    style: TextStyle(
+                                        color: Colors.white, fontFamily: 'SB'),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
